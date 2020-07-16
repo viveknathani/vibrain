6a8a2bca55cda281316173186ce270d284576d6d
viveknathani <viveknathani2402@gmail.com>
2020-07-16 20:53:13 +0530

    Insert/Get/Delete links

--- backend/models/links.js
+++ backend/models/links.js
@@ -1,12 +1,50 @@
 const mongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/';
-const DB_NAME = 'vibraindb';
-const COLL_NODES = 'nodes';
 const COLL_LINKS = 'links';
 
-function insertLink(){};
-function getAllLinks(){};
-function deleteLink(){};
+function insertLink(graphName, link)
+{
+    let creator = url + graphName;
+    mongoClient.connect(creator, function(err, client)
+    {
+        if(err) throw err.message;
+        let db = client.db(graphName);
+        db.collection(COLL_LINKS).insertOne(link, function(err, res)
+        {
+            if(err) throw err.message;
+            console.log('Node is inserted');
+            client.close();
+        });
+    });
+}
+
+function getAllLinks(graphName, allContent)
+{
+    let creator = url + graphName;
+    mongoClient.connect(creator, function(err, client)
+    {
+        if(err) throw err.message;
+        let db = client.db(graphName);
+        allContent = db.collection(COLL_LINKS).find({}).toArray();
+        client.close();
+    });
+}
+
+function deleteLink(graphName, link)
+{
+    let creator = url + graphName;
+    mongoClient.connect(creator, function(err, client)
+    {
+        if(err) throw err.message;
+        let db = client.db(graphName);
+        db.collection(COLL_LINKS).deleteOne(link, function(err, res)
+        {
+            if(err) throw err.message;
+            console.log('Link is deleted.');
+            client.close();
+        });
+    });
+}
 
 module.exports = {
     insertLink: insertLink,
