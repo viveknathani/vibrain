a770131d815008e22ae107a664e2f8fcee71797a
viveknathani <viveknathani2402@gmail.com>
2020-07-16 18:50:07 +0530

    Handle DB

--- backend/routes/control.js
+++ backend/routes/control.js
@@ -1,4 +1,5 @@
 const bodyParser = require('body-parser');
+const databaseHandler = require('../models/db');
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app)
@@ -6,5 +7,15 @@ module.exports = function(app)
     app.get('/', function(req, res)
     {
         res.send('This is vibrain.');
+    });
+
+    app.get('/about', function(req, res)
+    {
+        res.send('About.');
+    });
+
+    app.get('/workspace', function(req, res)
+    {
+        res.redirect('/');
     });
 }
