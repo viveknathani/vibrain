b292d92c748fdc0fdf1bb9dd7be64ce3f637d415
viveknathani <viveknathani2402@gmail.com>
2020-07-17 21:43:07 +0530

    N2N

--- backend/models/links.js
+++ backend/models/links.js
@@ -1,53 +1,16 @@
-const mongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/';
-const COLL_LINKS = 'links';
+/*
+    Schema for "links" in the database.
+    This facilitates the connection of two nodes.
+    The ids of two nodes are used to link them. 
+*/
 
-function insertLink(graphName, link)
+const mongoose = require('mongoose');
+
+const link = mongoose.Schema(
 {
-    let creator = url + graphName;
-    mongoClient.connect(creator, function(err, client)
-    {
-        if(err) throw err.message;
-        let db = client.db(graphName);
-        db.collection(COLL_LINKS).insertOne(link, function(err, res)
-        {
-            if(err) throw err.message;
-            console.log('Node is inserted');
-            client.close();
-        });
-    });
-}
+    sourceId: mongoose.Types.ObjectId,
+    targetId: mongoose.Types.ObjectId
+});
 
-function getAllLinks(graphName, allContent)
-{
-    let creator = url + graphName;
-    mongoClient.connect(creator, function(err, client)
-    {
-        if(err) throw err.message;
-        let db = client.db(graphName);
-        allContent = db.collection(COLL_LINKS).find({}).toArray();
-        client.close();
-    });
-}
-
-function deleteLink(graphName, link)
-{
-    let creator = url + graphName;
-    mongoClient.connect(creator, function(err, client)
-    {
-        if(err) throw err.message;
-        let db = client.db(graphName);
-        db.collection(COLL_LINKS).deleteOne(link, function(err, res)
-        {
-            if(err) throw err.message;
-            console.log('Link is deleted.');
-            client.close();
-        });
-    });
-}
-
-module.exports = {
-    insertLink: insertLink,
-    getAllLinks: getAllLinks,
-    deleteLink: deleteLink
-};
+module.exports = mongoose.model('link', link);
+    
