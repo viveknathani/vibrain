646912eebf99b42ca5e96e9472a6df4194dc0af4
viveknathani <viveknathani2402@gmail.com>
2020-07-17 21:42:44 +0530

    Connection

---
+++ backend/models/database.js
@@ -0,0 +1,16 @@
+/*
+    Connect to vibraindb and enable magic!
+*/
+
+const mongoose = require('mongoose');
+const mongoServerPath = 'mongodb://localhost:27017/vibraindb';
+const graphModel = require('./graph');
+
+function establishConnection()
+{
+    mongoose.connect(mongoServerPath,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Connected to the database.'))
+    .catch(err => console.log('Database connection error.'));
+}
+
+module.exports = { establishConnection: establishConnection };
