ddff420c6f73b6117c7dbb34a232704927cb78f3
viveknathani <viveknathani2402@gmail.com>
2020-07-17 21:44:10 +0530

    Simplified workflow

--- backend/api/control.js
+++ backend/api/control.js
@@ -1,21 +1,58 @@
 const bodyParser = require('body-parser');
-const databaseHandler = require('../models/db');
+const databaseHandler = require('../models/database');
 const urlencodedParser = bodyParser.urlencoded({extended: false});
+const graphModel = require('../models/graph');
 
 module.exports = function(app)
 {
+    databaseHandler.establishConnection();
+
     app.get('/', function(req, res)
     {
-        res.send('This is vibrain.');
+        res.send('This is vibrain\'s backend');
     });
 
-    app.get('/about', function(req, res)
+    app.get('/api/graphs', async function(req, res)
     {
-        res.send('About.');
+        try 
+        {
+            let all;
+            await graphModel.find()
+                .then(docs => all = JSON.stringify(docs))
+                .catch(err => console.error(err));         
+            res.send(all);
+        }
+        catch(err) 
+        {
+            res.status(500).json({ message: err.message });
+        }
     });
 
-    app.get('/workspace', function(req, res)
+    app.post('/api/graphs/:graph_name', async function(req, res)
     {
-        res.redirect('/');
+        try 
+        {
+            let creator = new graphModel({ name: req.params.graph_name });
+            await creator.save();
+            res.send('Created');
+        }
+        catch(err)
+        {
+            res.status(500).json({ message: err.message });
+        }
     });
+
+    app.delete('/api/graphs/:graph_name', async function(req, res)
+    {
+        try 
+        {
+            await graphModel.findOneAndRemove({name: req.params.graph_name});
+            res.send('Deleted');
+        }
+        catch(err)
+        {
+            res.status(500).json({ message: err.message });
+        }
+    });
+
 }
