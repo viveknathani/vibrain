f5c005b7655fdaad53ff2ff2fa94a62912da1250
viveknathani <viveknathani2402@gmail.com>
2020-07-17 21:43:27 +0530

    Schema for nodes

--- backend/models/nodes.js
+++ backend/models/nodes.js
@@ -1,71 +1,14 @@
-const mongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/';
-const COLL_NODES = 'nodes';
+/*
+    Schema for "nodes" in the database.
+    A node contains a string that can represent
+    absolutely anything.
+*/
 
-function insertNode(graphName, node)
+const mongoose = require('mongoose');
+
+const node = mongoose.Schema(
 {
-    let creator = url + graphName;
-    mongoClient.connect(creator, function(err, client)
-    {
-        if(err) throw err.message;
-        let db = client.db(graphName);
-        db.collection(COLL_NODES).insertOne(node, function(err, res)
-        {
-            if(err) throw err.message;
-            console.log('Node is inserted');
-            client.close();
-        });
-    });
-}
-
-function getAllNodes(graphName, allContent)
-{
-    let creator = url + graphName;
-    mongoClient.connect(creator, function(err, client)
-    {
-        if(err) throw err.message;
-        let db = client.db(graphName);
-        allContent = db.collection(COLL_NODES).find({}).toArray();
-        client.close();
-    });
-}
-
-function updateNode(graphName, node, newNode)
-{
-    let creator = url + graphName;
-    mongoClient.connect(creator, function(err, client)
-    {
-        if(err) throw err.message;
-        let db = client.db(graphName);
-        let currentNode = {_id: node._id};
-        db.collection(COLL_NODES).update(currentNode, newNode, function(err, res)
-        {
-            if(err) throw err.message;
-            console.log('Node is updated.');
-            client.close();
-        });
-    });
-}
-
-function deleteNode(graphName, node)
-{
-    let creator = url + graphName;
-    mongoClient.connect(creator, function(err, client)
-    {
-        if(err) throw err.message;
-        let db = client.db(graphName);
-        db.collection(COLL_NODES).deleteOne(node, function(err, res)
-        {
-            if(err) throw err.message;
-            console.log('Node is deleted.');
-            client.close();
-        });
-    });
-}
-
-module.exports = {
-    insertNode: insertNode,
-    getAllNodes: getAllNodes,
-    updateNode: updateNode,
-    deleteNode: deleteNode
-};
+    thought: String
+});
+    
+module.exports = mongoose.model('node', node);
