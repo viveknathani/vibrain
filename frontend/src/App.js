e9d41adf29ad499e5c990ed021390d70bcc3c180
viveknathani <viveknathani2402@gmail.com>
2020-07-18 21:07:45 +0530

    Add Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './components/header/header';
 
 class App extends React.Component
 {
   render()
   {
     return(
-      <h1>Basic.</h1>
+      <Header/>
     )
   }
 }; 
