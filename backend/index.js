19138296162f3c10c569f740f8b4c11e9fce2dbd
viveknathani <viveknathani2402@gmail.com>
2020-07-19 20:38:00 +0530

    CORS support

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const paths = require('./api/control');
 const port = 4000;
+const cors = require('cors');
 const app = express();
+app.use(cors());
 paths(app);
 
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}/`));
