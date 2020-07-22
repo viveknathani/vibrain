b932868d10d8ee7518066e2259b0ab24039dc31e
viveknathani <viveknathani2402@gmail.com>
2020-07-22 17:43:45 +0530

    Add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header/header';
 import GraphsList from './components/graph/graph';
+import Footer from './components/footer/footer';
 
 class App extends React.Component
 {
@@ -10,6 +11,7 @@ class App extends React.Component
       <div>
         <Header/>
         <GraphsList/>
+        <Footer/>
       </div>
     )
   }
