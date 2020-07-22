ba5362f7f5436e25bb6ed308adb98d8b9b5de7bf
viveknathani <viveknathani2402@gmail.com>
2020-07-22 17:44:11 +0530

    Footer component

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './footer.css';
+
+class Footer extends React.Component
+{
+    render()
+    {
+        return(
+            <div id="bottom">
+                <h4>Made with <i class="fa fa-heart" id="icon"></i> by Vivek Nathani</h4>
+            </div>
+        );
+    }
+};
+
+export default Footer;
