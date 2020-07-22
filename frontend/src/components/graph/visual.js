520deb0b36eb67ec2ca1c3dae2c8a730dc664c4b
viveknathani <viveknathani2402@gmail.com>
2020-07-22 18:31:47 +0530

    Cleaning up

--- frontend/src/components/graph/visual.js
+++ frontend/src/components/graph/visual.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import * as d3 from 'd3';
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
-
 class Visual extends React.Component 
 {
     create()
     {
-        d3.select("#parent").selectAll("svg").remove();
-        console.log(this.props.graph);
-        const width = 1000;
+        const width = 1000; 
         const height = 560;
+
+        // remove svg if exists, else multiple copies are created
+        d3.select("#parent").selectAll("svg").remove(); 
+
+        // creating svg, where the visualisation takes place
         const svg = d3.select("#parent").append("svg").attr("viewBox", [0, 0, width, height]);
+
+        // make an API call and create the network graph
         fetch('http://localhost:4000/api/graphs')
             .then(res => res.json())
             .then(data => 
                 {
                     let nodes, links = [];
+
+                    // find the data for the requested graph and fill the nodes and links
                     for(let i = 0; i < data.length; i++)
                     {
                         if(data[i].name === this.props.graph)
@@ -33,18 +36,15 @@ class Visual extends React.Component
                         }
                     }
 
-                    for(let i = 0; i < nodes.length; i++)
-                    {
-                        nodes[i].group = getRndInteger(1, 5);
-                    }
+                    let colorS = d3.scaleOrdinal(d3.schemeCategory10);
 
-                    let colors = ['#FF7B00', '#E8FF00', '#59FF00', '#004DFF'];
+                    // provide data and call d3 force layout functions to simulate
+                    const simulation = d3.forceSimulation(nodes)
+                                        .force('links', d3.forceLink(links).distance(160).id(d => {return d._id}))
+                                        .force("charge", d3.forceManyBody())
+                                        .force("center", d3.forceCenter(width / 2, height / 2));
 
-                    console.log(nodes);
-
-
-
-                    const simulation = d3.forceSimulation(nodes).force('links', d3.forceLink(links).distance(160).id(d => {return d._id})).force("charge", d3.forceManyBody()).force("center", d3.forceCenter(width / 2, height / 2));
+                    // add dragging ability                                                
                     const drag = simulation => {
   
                         function dragstarted(d) {
@@ -69,20 +69,49 @@ class Visual extends React.Component
                             .on("drag", dragged)
                             .on("end", dragended);
                       }
-                
-                      const link = svg.append("g").attr("stroke", "#888").attr("stroke-opacity", 0.6).selectAll("line").data(links).join("line");
-                      const node = svg.append("g").selectAll("circle").data(nodes).join("circle").attr("r", 40).attr("fill", () => { let i = getRndInteger(0, 4); return colors[i];})
-                      .on("contextmenu", function (d, i) {
-                        d3.event.preventDefault();
-                        d3.select("#root").select("p").remove();
-                       d3.select("#root").append("p").text("selected node's id: "+d._id);
-                    }).call(drag(simulation));
-                       const text = svg.append("g").selectAll("text").data(nodes).join("text").attr("color", "blue");
-                       simulation.on("tick", () => {
-                           link.attr("x1", d => d.source.x).attr("y1", d => d.source.y).attr("x2", d => d.target.x).attr("y2", d => d.target.y);
-                           node.attr("cx", d => d.x).attr("cy", d => d.y);
-                           text.attr("x", d => d.x).attr("y", d => d.y).text(d => d.thought).attr("font-size", "15px").attr("font-weight", "bold").attr('text-anchor', 'middle')
-                           .attr('alignment-baseline', 'middle')
+  
+                      
+                      // details for a link
+                      const link = svg.append("g")
+                                    .attr("stroke", "#888")
+                                    .attr("stroke-opacity", 0.6)
+                                    .selectAll("line").data(links).join("line");
+
+                      // details for a node                                    
+                      const node = svg.append("g")
+                                    .selectAll("circle").data(nodes).join("circle")
+                                    .attr("r", 40)
+                                    .attr("fill", (d, i) => colorS(i))                            
+                                    .on("contextmenu", (d, i) => 
+                                    {
+                                        // on right-click, show node id
+                                        d3.event.preventDefault();
+                                        d3.select("#root").select("p").remove();
+                                        d3.select("#root").append("p").text("selected node's id: "+d._id);
+                                    })
+                                    .call(drag(simulation));
+
+                       // details for text inside a node             
+                      const text = svg.append("g").selectAll("text").data(nodes).join("text");
+
+                      // calculate co-ordinates for nodes and links during the runtime of simulation
+                       simulation.on("tick", () => 
+                       {
+                           link.attr("x1", d => d.source.x)
+                                .attr("y1", d => d.source.y)
+                                .attr("x2", d => d.target.x)
+                                .attr("y2", d => d.target.y);
+
+                           node.attr("cx", d => d.x)
+                                .attr("cy", d => d.y);
+
+                           text.attr("x", d => d.x)
+                                .attr("y", d => d.y)
+                                .text(d => d.thought)
+                                .attr("font-size", "15px")
+                                .attr("font-weight", "bold")
+                                .attr('text-anchor', 'middle')
+                                .attr('alignment-baseline', 'middle')
                        });
                       
                 });
@@ -96,7 +125,7 @@ class Visual extends React.Component
     render()
     {
         return(
-            <div id="#node_info">
+            <div>
             </div>
         );
     }
